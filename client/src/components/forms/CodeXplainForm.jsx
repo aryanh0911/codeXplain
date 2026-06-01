20217661c7a02a1a28adc5d896ff00aeb11227f4
enceladus <aryanhazarika04@gmail.com>
2026-06-02 02:27:05 +0530

    ADD: languages

--- client/src/components/forms/CodeXplainForm.jsx
+++ client/src/components/forms/CodeXplainForm.jsx
@@ -26,9 +26,14 @@ const CodeXplainForm = () => {
                                 className="w-full appearance-none bg-black/20 border border-white/10 rounded-xl px-4 py-3 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500/50 focus:border-indigo-500/50 transition-all cursor-pointer"
                             >
                                 <option value="javascript" className="bg-gray-900">JavaScript</option>
+                                <option value="typescript" className="bg-gray-900">TypeScript</option>
                                 <option value="python" className="bg-gray-900">Python</option>
-                                <option value="c++" className="bg-gray-900">C++</option>
                                 <option value="java" className="bg-gray-900">Java</option>
+                                <option value="c" className="bg-gray-900">C</option>
+                                <option value="c++" className="bg-gray-900">C++</option>
+                                <option value="c#" className="bg-gray-900">C#</option>
+                                <option value="go" className="bg-gray-900">Go</option>
+                                <option value="Rust" className="bg-gray-900">Rust</option>
                             </select>
                             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-gray-400">
                                 <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
