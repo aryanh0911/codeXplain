e0b963bef8d46e13d157f04fdd90902b8c7cee28
enceladus <aryanhazarika04@gmail.com>
2026-06-01 21:17:44 +0530

    FIX: code persistency in text-area after form submission

--- client/src/components/forms/CodeSplainForm.jsx
+++ client/src/components/forms/CodeSplainForm.jsx
@@ -1,10 +1,11 @@
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 import { explain } from "../../actions";
 import CodeExplanation from "../CodeExplanation";
 import Error from "../Error";
 
 const CodeSplainForm = () => {
     const [result, submitAction, isPending] = useActionState(explain, null)
+    const [code, setCode] = useState("");
 
     return(
         <div className="w-full max-w-4xl  p-6 rounded-2xl shadow-lg">
@@ -18,7 +19,13 @@ const CodeSplainForm = () => {
                 </select>
 
                 <label className="block mb-2 font-semibold"> Your Code: </label>
-                <textarea name="code" placeholder="Paste your code here" className="border rounded-lg w-full p-3 font-mono text-sm bg-transparent min-h-[150px]" />
+                <textarea 
+                    name="code"
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    placeholder="Paste your code here" 
+                    className="border rounded-lg w-full p-3 font-mono text-sm bg-transparent min-h-[150px]"
+                />
 
                 <button
                     type="submit"
