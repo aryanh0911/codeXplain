a0ebe1f29b1d322504964b4464719b345c6d46bf
enceladus <aryanhazarika04@gmail.com>
2026-06-01 19:23:32 +0530

    ADD: switched from fetch (dumb) to axios

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,24 +1,19 @@
 "use server"
 
+import axios from 'axios'
+
 export async function explain (prevState, formData){
+    
     const code = formData.get("code");
     const language = formData.get("language");
 
     try {
-        const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/explain-code`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({code, language}),
+        const res = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/explain-code`, {
+            code,
+            language
         })
         
-        if(!res.ok){
-            return {
-                success: false,
-                error: "Failed to fetch results.",
-            }
-        }
-
-        const data = await res.json();
+        const data = res.data;
         
         return {
             success: true,
